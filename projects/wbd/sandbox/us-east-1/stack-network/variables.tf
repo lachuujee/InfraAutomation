158b6e180723f0cb8b1843e07cebeeb1a1325bdf
lachuujee <lakshmi.wl240@gmail.com>
2025-09-20 18:01:29 +0530

    Update variables.tf

--- projects/wbd/sandbox/us-east-1/stack-network/variables.tf
+++ projects/wbd/sandbox/us-east-1/stack-network/variables.tf
@@ -1,7 +1,19 @@
-variable "customer"    { type = string }                 # "WBD"
-variable "environment" { type = string }                 # "sandbox" | "prod"
-variable "region"      { type = string }                 # "us-east-1"
-variable "vpc_cidr"    { type = string, default = "10.20.0.0/16" }
+variable "customer" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.20.0.0/16"
+}
 
 locals {
   name_prefix = "${var.customer}_${var.environment}"
