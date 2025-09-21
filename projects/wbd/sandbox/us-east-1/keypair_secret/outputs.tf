a42a6f42af58eca3ac3bf739e9191b1e49c9a771
lachuujee <lakshmi.wl240@gmail.com>
2025-09-21 23:55:15 +0530

    Update outputs.tf

--- projects/wbd/sandbox/us-east-1/keypair_secret/outputs.tf
+++ projects/wbd/sandbox/us-east-1/keypair_secret/outputs.tf
@@ -1,3 +1,11 @@
-output "key_name"    { value = aws_key_pair.this.key_name }
-output "secret_name" { value = aws_secretsmanager_secret.pk.name }
-output "secret_arn"  { value = aws_secretsmanager_secret.pk.arn }
+output "key_name" {
+  value = aws_key_pair.this.key_name
+}
+
+output "secret_name" {
+  value = aws_secretsmanager_secret.pk.name
+}
+
+output "secret_arn" {
+  value = aws_secretsmanager_secret.pk.arn
+}
