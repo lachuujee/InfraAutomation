335768e9268d27718e37371d38005b50a38a2b6b
lachuujee <lakshmi.wl240@gmail.com>
2025-09-21 22:58:59 +0530

    Update providers.tf

--- projects/wbd/sandbox/us-east-1/keypair_secret/providers.tf
+++ projects/wbd/sandbox/us-east-1/keypair_secret/providers.tf
@@ -1,12 +1,17 @@
 terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.60"
+    }
+  }
   backend "s3" {
-    bucket         = "YOUR_TF_STATE_BUCKET"
-    key            = "projects/wbd/sandbox/keypair_secret/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
+    bucket = "wbd-tf-state-sandbox"                 # <-- your bucket
+    key    = "wbd/sandbox/keypair_secret/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
   }
 }
 
-provider "aws" {
-  region = var.region
-}
+provider "aws" { region = var.region }
